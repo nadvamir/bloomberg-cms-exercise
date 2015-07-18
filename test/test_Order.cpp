eb5602b38c461e7dbd48a0779898f7dd0cacd525
Maksim Solovjov <himaxx2@gmail.com>
2015-07-18 18:48:01 +0100

    Pass An order has remaining quantity

--- test/test_Order.cpp
+++ test/test_Order.cpp
@@ -2,6 +2,7 @@
 
 #include "include/Commodity.h"
 
+using namespace testing;
 using namespace std;
 
 class Order {
@@ -10,22 +11,31 @@ public:
 
 private:
     Side side_;
+    int quantity_;
 
 public:
-    Order(Side s) : side_(s) {}
+    Order(Side s, int q)
+    : side_(s), quantity_(q) {}
 
     Side side() const { return side_; }
+    int quantity() { return quantity_; }
 };
 
 TEST(AnOrder, CanBeBought) {
-    Order order(Order::Buy);
+    Order order(Order::Buy, 1);
 
-    ASSERT_TRUE(order.side() == Order::Buy);
+    ASSERT_THAT(order.side(), Eq(Order::Buy));
 }
 
 TEST(AnOrder, CanBeSold) {
-    Order order(Order::Sell);
+    Order order(Order::Sell, 1);
 
-    ASSERT_TRUE(order.side() == Order::Sell);
+    ASSERT_THAT(order.side(), Eq(Order::Sell));
 }
 
+TEST(AnOrder, HasRemainingQuantity) {
+    int quantity = 500;
+    Order order(Order::Sell, quantity);
+
+    ASSERT_THAT(order.quantity(), Eq(quantity));
+}
