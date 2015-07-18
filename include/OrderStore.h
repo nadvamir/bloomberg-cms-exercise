78d2a5549a9381ed9c767ac0aadfcb68a6a02f0b
Maksim Solovjov <himaxx2@gmail.com>
2015-07-19 00:07:41 +0100

    Pass An OrderStore stores unique ids to every order

--- include/OrderStore.h
+++ include/OrderStore.h
@@ -9,10 +9,13 @@
 class OrderStore {
     typedef std::map<long, OrderPtr> OrderMap;
     OrderMap store;
+    long lastId;
     
 public:
+    OrderStore() : lastId(0) {}
+
     long put(OrderPtr& order) {
-        long id = 0;
+        long id = ++lastId;
         order->id(id);
         store.insert(std::make_pair(id, order));
         return id;
--- test/test_OrderStore.cpp
+++ test/test_OrderStore.cpp
@@ -20,3 +20,21 @@ TEST(AnOrderStore, StoresOrdersAttachingIdsToThem) {
 
     ASSERT_THAT(store.get(id)->id(), Eq(id));
 }
+
+TEST(AnOrderStore, StoresUniqueIdsToEveryOrder) {
+    OrderStore store;
+    OrderPtr order = OrderPtr(new Order(
+        Dealer("JPM"), Order::Sell,
+        CommodityPtr(new Gold),
+        100, 59.99));
+
+    OrderPtr order2 = OrderPtr(new Order(
+        Dealer("JPM"), Order::Sell,
+        CommodityPtr(new Gold),
+        100, 59.99));
+
+    long id = store.put(order);
+    long id2 = store.put(order2);
+
+    ASSERT_THAT(id, Ne(id2));
+}
