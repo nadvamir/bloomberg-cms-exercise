28c5dd423f29eaf1f92acb352ffd1c75e0f1fc46
Maksim Solovjov <himaxx2@gmail.com>
2015-07-18 19:08:31 +0100

    Pass An order must have a positive price

--- include/Order.h
+++ include/Order.h
@@ -10,11 +10,12 @@ public:
 private:
     Side side_;
     int quantity_;
+    double price_;
 
 public:
-    Order(Side s, int q)
-    : side_(s), quantity_(q) {
-        if (q < 1) {
+    Order(Side s, int q, double p)
+    : side_(s), quantity_(q), price_(p) {
+        if (q < 1 || p <= 0.0) {
             throw InvalidMessage();
         }
     }
@@ -22,6 +23,5 @@ public:
     Side side() const { return side_; }
     int quantity() { return quantity_; }
 };
-
 
 #endif
--- test/test_Order.cpp
+++ test/test_Order.cpp
@@ -7,27 +7,43 @@
 using namespace testing;
 using namespace std;
 
-TEST(AnOrder, CanBeBought) {
-    Order order(Order::Buy, 1);
+class AnOrder : public Test {
+public:
+    int quantity;
+    float price;
+
+    void SetUp() {
+        quantity = 500;
+        price = 1.0;
+    }
+};
+
+TEST_F(AnOrder, CanBeBought) {
+    Order order(Order::Buy, 1, price);
 
     ASSERT_THAT(order.side(), Eq(Order::Buy));
 }
 
-TEST(AnOrder, CanBeSold) {
-    Order order(Order::Sell, 1);
+TEST_F(AnOrder, CanBeSold) {
+    Order order(Order::Sell, 1, price);
 
     ASSERT_THAT(order.side(), Eq(Order::Sell));
 }
 
-TEST(AnOrder, HasRemainingQuantity) {
-    int quantity = 500;
-    Order order(Order::Sell, quantity);
+TEST_F(AnOrder, HasRemainingQuantity) {
+    Order order(Order::Sell, quantity, price);
 
     ASSERT_THAT(order.quantity(), Eq(quantity));
 }
 
-TEST(AnOrder, MustHaveAPositiveQuantity) {
-    ASSERT_NO_THROW(Order(Order::Sell, 1));
-    ASSERT_THROW(Order(Order::Sell, 0), InvalidMessage);
-    ASSERT_THROW(Order(Order::Sell, -1), InvalidMessage);
+TEST_F(AnOrder, MustHaveAPositiveQuantity) {
+    ASSERT_NO_THROW(Order(Order::Sell, 1, price));
+    ASSERT_THROW(Order(Order::Sell, 0, price), InvalidMessage);
+    ASSERT_THROW(Order(Order::Sell, -1, price), InvalidMessage);
+}
+
+TEST_F(AnOrder, MustHaveAPositivePrice) {
+    ASSERT_NO_THROW(Order(Order::Sell, quantity, 1.0));
+    ASSERT_THROW(Order(Order::Sell, quantity, 0.0), InvalidMessage);
+    ASSERT_THROW(Order(Order::Sell, quantity, -1.0), InvalidMessage);
 }
