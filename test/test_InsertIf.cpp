bb2812ddcfde936bb0ef4617152478105aded8ee
Maksim Solovjov <himaxx2@gmail.com>
2015-07-19 12:10:20 +0100

    Pass An IfInserter can be created with types deduced

--- test/test_InsertIf.cpp
+++ test/test_InsertIf.cpp
@@ -20,6 +20,15 @@ public:
     }
 };
 
+template<class Element>
+struct IfInserter {
+    template<class Pred, class Inserter>
+    static InsertIf<Element, Pred, Inserter>
+    make(Pred pred, Inserter inserter) {
+        return InsertIf<Element, Pred, Inserter>(pred, inserter);
+    }
+};
+
 bool even(int num) { return num % 2 == 0; }
 
 TEST(AnInsertIf, CopiesElementsIfTheyMatchAPredicate) {
@@ -32,10 +41,28 @@ TEST(AnInsertIf, CopiesElementsIfTheyMatchAPredicate) {
     copy(evenVals, evenVals + 2, back_inserter(evenValv));
 
     vector<int> evenv;
-    InsertIf<int, bool (*)(int), back_insert_iterator<vector<int> > > iif(
-            even, back_inserter(evenv));
+    InsertIf<int, bool (*)(int),
+             back_insert_iterator<vector<int> >
+    > iif(even, back_inserter(evenv));
 
     for_each(vals, vals + 4, iif);
+
+    ASSERT_THAT(evenv, Eq(evenValv));
+}
+
+TEST(AnInsertIf, CanBeCreatedWithTypesDeduced) {
+    int vals[] = {4, 5, 5, 4};
+    int evenVals[] = {4, 4};
+
+    vector<int> valv;
+    copy(vals, vals + 4, back_inserter(valv));
+    vector<int> evenValv;
+    copy(evenVals, evenVals + 2, back_inserter(evenValv));
+
+    vector<int> evenv;
+
+    for_each(vals, vals + 4,
+             IfInserter<int>::make(even, back_inserter(evenv)));
 
     ASSERT_THAT(evenv, Eq(evenValv));
 }
