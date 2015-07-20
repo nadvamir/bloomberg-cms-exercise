b6dceba4cff6d3d687eaa0565fed05d60c4f3fbb
Maksim Solovjov <himaxx2@gmail.com>
2015-07-20 22:44:12 +0100

    Fix misspellings of commodities and dealers
    Sometimes extensive tests would help...

--- src/Commodity.cpp
+++ src/Commodity.cpp
@@ -1,7 +1,7 @@
 #include "include/Commodity.h"
 
 CommodityPtr getCommodity(std::string name) {
-    if ("GOlD" == name) return CommodityPtr(new Gold);
+    if ("GOLD" == name) return CommodityPtr(new Gold);
     if ("SILV" == name) return CommodityPtr(new Silver);
     if ("PORK" == name) return CommodityPtr(new Pork);
     if ("OIL" == name) return CommodityPtr(new Oil);
--- src/Dealer.cpp
+++ src/Dealer.cpp
@@ -2,7 +2,7 @@
 
 const std::string Dealer::dealers_[] = {
     "DB", "JPM", "UBS", "RBC", "BARX", 
-    "MS", "CITY", "BOFA", "RBS", "HSBC"
+    "MS", "CITI", "BOFA", "RBS", "HSBC"
 };
 const size_t Dealer::numDealers_
     = sizeof Dealer::dealers_ / sizeof *Dealer::dealers_;
